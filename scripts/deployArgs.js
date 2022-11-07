809503043bf69908d56717c52935c2012b67aabc
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-11-06 20:33:20 -0600

    Initial

---
+++ scripts/deployArgs.js
@@ -0,0 +1,18 @@
+module.exports = [
+ 'Rockin Skulls NFT',
+ 'RSNFT',
+ 
+    '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
+    '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
+    'https://public-ipfs.s3.us-east-2.amazonaws.com/RockinSkullsNFT/json/',
+    'https://public-ipfs.s3.us-east-2.amazonaws.com/RockinSkullsNFT/assets/rs.json',
+
+    [
+        ethers.utils.getAddress('0x2B40eD959FB5dE183Eb7f99C5a4c3336F5C76127'),
+        ethers.utils.getAddress('0xCd43AdcB61949ab14D3f4574BFbDA53d46389715')
+    ],
+    [
+        50,
+        50
+    ]
+]
